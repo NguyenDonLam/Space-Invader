033a9a0cf9955382db5f7396baa623bd3feaebb5
donlamn <donlam.nguyen@unimelb.edu.au>
2023-08-31 23:22:00 +1000

    First commit that work, Still have to finish up MultipleAlien, try to delegate new row generation to MultipleAlien from SpaceInvader if possible

--- app/src/main/java/AlienGridLocation.java
+++
@@ -1,9 +0,0 @@
-public class AlienGridLocation {
-    public int rowIndex;
-    public int colIndex;
-
-    public AlienGridLocation(int rowIndex, int colIndex) {
-        this.rowIndex = rowIndex;
-        this.colIndex = colIndex;
-    }
-}
